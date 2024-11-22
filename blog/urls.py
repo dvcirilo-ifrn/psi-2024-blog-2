50c333dc502014c248d8cd9aeacbaa48178c3a1d
Diego Cirilo <dvcirilo@gmail.com>
2024-11-22 10:24:29 -0300

    update 22-11

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,13 @@ urlpatterns = [
     path('post/<int:post_id>', views.post, name='post'),
     path('sobre/', views.sobre, name='sobre'),
     path('contato/', views.contato, name='contato'),
+    path('mensagens/', views.mensagens, name='mensagens'),
+    path('mensagens/<int:mensagem_id>/editar/',
+         views.editar_mensagem,
+         name='editar_mensagem'
+        ),
+    path('mensagens/<int:mensagem_id>/deletar/',
+         views.deletar_mensagem,
+         name='deletar_mensagem'
+        ),
 ]
